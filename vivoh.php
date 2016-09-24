eb18742595deb2f2bd3cbb13635821995fae42e1
jdcotes <jdcotes@uninorte.edu.co>
2016-09-24 15:19:21 -0500

    hol

--- vivoh.php
+++ vivoh.php
@@ -7,7 +7,7 @@
   mysql_select_db($database,$con)or die("No se encontró la base de datos");
   
 
-  $sql = "INSERT INTO Pokemap('IDPok', 'Level', 'Attack 1', 'Attack 2', 'Latitud', 'Longitud', 'Flag') VALUES('$_POST[idb]', 50, 0, 0, '$_POST[latb]', '$_POST[lngb]',0) ";
+  $sql = "INSERT INTO Pokemap('IDPok', 'Level', 'Attack 1', 'Attack 2', 'Latitud', 'Longitud', 'Flag') VALUES('$_POST[idb]','50', '0', '0', '$_POST[latb]', '$_POST[lngb]','0') ";
   $ejecutar_sql1=mysql_query($sql) or die("Problemas en consulta: ".mysql_error());
   echo("mande a base");
   mysql_free_result($ejecutar_sql1);
